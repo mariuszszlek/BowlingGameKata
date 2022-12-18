f411df3e97b069cb0333a9e7e2706c4444438309
mariuszszlek <mariuszszlek@gmail.com>
2022-12-18 10:12:18 +0100

    First commit added scored spare

--- src/main/java/org/bmc/tdd/BowlingGame.java
+++ src/main/java/org/bmc/tdd/BowlingGame.java
@@ -2,7 +2,25 @@ package org.bmc.tdd;
 
 public class BowlingGame {
 
+    int roll = 0;
+    private int[] rolls = new int[21];
+
     public int getScore() {
-        return -1;
+        int score = 0;
+        int coursor = 0;
+        for (int i = 0; i < 10; i++) {
+            if (rolls[coursor] + rolls[coursor+1] == 10) {
+                score += 10 + rolls[coursor+2];
+                coursor +=2;
+            } else{
+                score += rolls[coursor] + rolls[coursor+1];
+                coursor+=2;
+            }
+        }
+        return score;
+    }
+
+    public void roll(int pins) {
+        rolls[roll++] = pins;
     }
 }
--- src/test/java/org/bmc/tdd/BowlingGameTest.java
+++ src/test/java/org/bmc/tdd/BowlingGameTest.java
@@ -1,23 +1,53 @@
 package org.bmc.tdd;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class BowlingGameTest {
 
-    BowlingGame theGame = new BowlingGame();
+    BowlingGame theGame;
 
-    @Test
-    public void shouldScoreOneWhenRolledOneWithJUnit() {
-        assertEquals(1, theGame.getScore());
+    @BeforeEach
+    void setUp() {
+        theGame = new BowlingGame();
     }
 
     @Test
-    public void shouldScoreOneWhenRolledOneWithAssertJ() {
-        Assertions.assertThat(theGame.getScore()).isEqualTo(1);
+    public void shouldScoreOneWhenNoRolls() {
+
+        assertThat(theGame.getScore()).isEqualTo(0);
     }
 
+    @Test
+    void shouldScore0WhenNoPinDown20Times() {
+        int numberOfThrows = 20;
+        int pins = 0;
+        roll(numberOfThrows, pins);
+        assertThat(theGame.getScore()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldScore20WhenOnePinDown20Times() {
+        roll(20, 1);
+        assertThat(theGame.getScore()).isEqualTo(20);
+    }
+
+    @Test
+    void shouldScoreSpere() {
+        roll(2,5);
+        roll(1,4);
+        roll(17,0);
+        assertThat(theGame.getScore()).isEqualTo(18);
+    }
+
+    private void roll(int numberOfThrows, int pins) {
+        for (int i = 0; i < numberOfThrows; i++) {
+            theGame.roll(pins);
+        }
+    }
 }
